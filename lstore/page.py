03cbf22f03558a425ac9ad22a5cc37b3f78ecccb
Gavin Lang <gavincollislang@gmail.com>
2025-01-26 20:10:58 -0800

    implemented basic page write and has_capacity

--- lstore/page.py
+++ lstore/page.py
@@ -1,13 +1,21 @@
 
+PAGE_SIZE = 4096
 class Page:
 
-    def __init__(self):
+    def __init__(self, record_size=8):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.record_size = record_size
+        self.data = bytearray(PAGE_SIZE)
 
     def has_capacity(self):
-        pass
+        if self.num_records * self.record_size < PAGE_SIZE:
+            return True
+        return False
 
     def write(self, value):
+        bytes = value.to_bytes(8)
+        index = self.num_records * 8
+        self.data[index: index + 8] = bytes
         self.num_records += 1
-        pass
+        return index
+
