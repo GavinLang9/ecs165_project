7329a146386d0b0c69ae7f318c906f2a7bf555ce
Connor Atha <connoratha@gmail.com>
2025-02-01 15:49:26 -0800

    Added basic index.py functionality

--- lstore/index.py
+++ lstore/index.py
@@ -1,20 +1,28 @@
 """
-A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
+A data structure holding indices for various columns of a table. Key column should be indexed by default, other columns
+can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
 """
+
 
 class Index:
 
     def __init__(self, table):
-        # One index for each table. All our empty initially.
-        self.indices = [None] *  table.num_columns
-        pass
+        # One index for each table. All are empty initially.
+        self.indices = [None] * table.num_columns
+
+        # Index primary key column
+        self.create_index( table.key_idx )
 
     """
     # returns the location of all records with the given value on column "column"
     """
 
     def locate(self, column, value):
-        pass
+        if self.indices[column] is None:
+            return None
+
+        result = self.indices[column].get( value )
+        return result
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
@@ -28,7 +36,11 @@ class Index:
     """
 
     def create_index(self, column_number):
-        pass
+        # exit if column index already exists
+        if self.indices[column_number] is not None:
+            return
+
+        self.indices[column_number] = {}
 
     """
     # optional: Drop index of specific column
