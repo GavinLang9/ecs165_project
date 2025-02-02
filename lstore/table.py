a2f9cd370fbe3a5d8875267861afe28dbb2154d1
Gavin Lang <gavincollislang@gmail.com>
2025-02-01 16:22:31 -0800

    working table for relatively large data.  Doesn't work after bufferpool is full.  Need to fix evict function

--- lstore/table.py
+++ lstore/table.py
@@ -1,7 +1,10 @@
 from typing import Tuple
 from lstore.index import Index
 from lstore.bufferpool import BufferPool
+from lstore.disk import Disk
+from lstore.page import Page
 from time import time
+
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
@@ -11,6 +14,7 @@ SCHEMA_ENCODING_COLUMN = 3
 BUFFER_POOL_CAPACITY = 16
 RECORDS_PER_PAGE = 512
 PAGE_RANGE_MAX_LEN = 64
+NUM_META_COLUMNS = 4
 
 class Record:
 
@@ -30,63 +34,74 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
-        self.page_directory = {}    # RID -> (page_range_id, page_id) ?
-        self.bufferpool = BufferPool(BUFFER_POOL_CAPACITY)
+        self.page_directory = {}    # RID -> ([page_range_ids], [page_ids], [offsets])
+        self.disk = Disk()
+        self.bufferpool = BufferPool(BUFFER_POOL_CAPACITY, self.disk)
         self.index = Index(self)
         self.rid_counter = 0
         self.current_page_range = 0
         self.current_page = 0   # page id of first column page
-
+        self.current_offset = 0
         pass
 
-    def create_record(self, key, columns):
+    def create_record(self, columns):
         """
         Creates a new record by writing columns to their corresponding pages.
         
         Args:
             key (int): Primary key value
             columns (list[int]): List of column values to write
-            
+                first column is key column (usually)
+                [key, col 1, col 2, col 3, ...]
         """
         # create meta data columns
-        if len(columns != self.num_columns):
+        if len(columns) != self.num_columns:  
             raise ValueError("Invalid number of columns")
         rid = self.rid_counter
         metadata = [
             rid,                            # INDIRECTION
             rid,                            # RID
             int(time() * 1000),             # TIMESTAMP
-            '0' * self.num_columns,          # SCHEMA ENCODING
-            key
+            0,                              # SCHEMA ENCODING
         ]
         record_data =  metadata + columns
 
         # returns a tuple of lists that hold page range and page indexes for each column
         page_range_ids, column_page_ids = self._get_write_locations()
+
         offsets = []
         # write each column to their correspoding location in disk
         for value, page_range_id, page_id in zip(record_data, page_range_ids, column_page_ids):
             page = self.bufferpool.get_page(page_range_id, page_id)
+            if not page:
+                page = Page()
             if not page.has_capacity():
+                print(page_range_ids, column_page_ids)
                 raise IndexError("This page has no space")
             index = page.write(value)
+
             offsets.append(index)
-            self.bufferpool.write(page_range_id, page_id, page)
-        
+            self.bufferpool.write_page(page_range_id, page_id, page)
         # TODO: Maybe call bufferpool.flush to commit
         # Update table metadata
         # TODO: Fix page directory to include offset in page and update this everywhere else
         self.page_directory[rid] = (page_range_ids, column_page_ids, offsets)
-        self._update_indexes(rid)
+        self._update_indexes()
         
-    def get_record(self, rid: int, projected_columns_index: list[int]):
+    def get_record(self, rid: int) -> Record:
+        """
+        returns constructed record by getting each column value from their respective pages
+        """
         page_range_ids, page_ids, offsets = self.page_directory[rid]
         columns = []
         for page_range_id, page_id, offset in zip(page_range_ids, page_ids, offsets):
             page = self.bufferpool.get_page(page_range_id, page_id)
             value = page[offset]
             columns.append(value)
+            
         # TODO: Continue working on get_record 
+        record = Record(rid, columns[self.key + NUM_META_COLUMNS], columns[5:]) # 4 columns of metadata followed by key
+        return record
 
     def _new_pages_will_overflow_page_range(self, total_columns):
         """
@@ -110,12 +125,12 @@ class Table:
                 - List of page_ids for each column
         """
         # Default all columns will be written to self.current_page_range
-        total_columns = self.num_columns + 4
+        total_columns = self.num_columns + NUM_META_COLUMNS
         page_range_ids = [self.current_page_range] * total_columns
-        column_page_ids = [self.current_page] * total_columns
+        column_page_ids = list(range(self.current_page, self.current_page + total_columns))
         
         # If no overflow, all columns go to current page range
-        if not self._new_pages_will_overflow_page_range():
+        if not self._new_pages_will_overflow_page_range(total_columns):
             return (page_range_ids, column_page_ids)
 
         # Else, split columns into remaining space and next page range
@@ -134,20 +149,32 @@ class Table:
         
         return (page_range_ids, column_page_ids)
 
-    def _update_indexes(self, rid: int):
+    def _update_indexes(self):
         """
         Update table counters for  
             page range  
             page id  
+            offset
             rid  
         """
-        if rid % (RECORDS_PER_PAGE - 1) == 0:
-            if self.current_page == PAGE_RANGE_MAX_LEN - 1:
+        total_columns = self.num_columns + NUM_META_COLUMNS
+
+        if self._page_is_full() and self.current_offset != 0:
+            if self._page_range_is_full(total_columns):
                 self.current_page_range += 1
-                self.current_page = 0
-            else:
-                self.current_page += 1
+            new_page_id =  (self.current_page + total_columns) % PAGE_RANGE_MAX_LEN
+            self.current_page = new_page_id
+            self.current_offset = 0
         self.rid_counter += 1
+        self.current_offset += 1
+
+    # Checks if page range is full based on page_id counter
+    def _page_range_is_full(self, total_columns):
+        return self.current_page > PAGE_RANGE_MAX_LEN - total_columns
+
+    # Checks if page is full based on offset counter
+    def _page_is_full(self):
+        return self.current_offset % (RECORDS_PER_PAGE - 1) == 0
 
     def __merge(self):
         print("merge is happening")
