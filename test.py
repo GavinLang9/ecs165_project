ead77cb21ec36deec4c5eee626832fb7f709bd32
mcbali <mcrbalingit@gmail.com>
2025-02-09 03:02:40 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,25 @@
-
 from lstore.table import Table
+from lstore.query import Query
+from lstore.query import Index
 import pdb
+
 table = Table('test', 3, 0)
 
-num = 10000
-
+num = 10
+query = Query(table)
 for i in range(0,num):
-  table.create_record([i+1, i+1, i+1])
-
+  testInsert = query.insert(i+1, i+10, i+100)
 
 rid = 'rid'
-key='key'
+key = 'key'
 print(f'{rid:<{5}} {key:<{5}} columns\n')
 
-for i in range(0,num, 1000):
+for i in range(0,num ):
   rec = table.get_latest_record(i)
   print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
 
 # print(table.disk)
-
+"""
 print('\n')
 for i in range(0,num):
   table.update_record(i, [None, i + 2, i + 3])
@@ -32,12 +33,29 @@ for i in range(0,num, 1000):
 #   pdb.set_trace()
   rec = table.get_latest_record(i)
   print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
+"""
 
-# print(table.disk)
-# tu = (None, None, 1, 0, None)
-# l = sum(1 for x in tu if x is not None)
-# print(l)
+success1 = query.insert(11, 12, 13)
+# print(success1)
+#table.create_record([10001, 2002, 3003])
+rec = table.get_latest_record(10)
+print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
 
-# up = list(0 for x in tu if x is not None else None)
-# print(up)
-#dd
+#success2 = query.insert(10002, 2002, 4003)
+#table.create_record([10002, 2002, 4003])
+success2 = query.insert(12, 13, 14)
+rec = table.get_latest_record(11)
+print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
+print(f"\n")
+
+# Test Locate\
+locate_column = 1
+locate_key = 10
+locate_records = query.index.indices[locate_column].get(locate_key)
+print("USING INDEX LOCATE:")
+print(f"RID's where column {locate_column} is {locate_key}: {locate_records}\n")
+
+# Test Select
+select_records = query.select(locate_key, locate_column, [1, 1, 1])
+print("USING SELECT:")
+print(f"RID's where column {locate_column} is {locate_key}: {select_records}\n")
