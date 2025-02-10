1d82e7d25859ef523a56af162d9f7e922215856a
mcbali <mcrbalingit@gmail.com>
2025-02-09 22:50:22 -0800

    Update test.py

--- test.py
+++ test.py
@@ -7,55 +7,71 @@ table = Table('test', 3, 0)
 
 num = 10
 query = Query(table)
+
+# Test Insert
 for i in range(0,num):
   testInsert = query.insert(i+1, i+10, i+100)
 
 rid = 'rid'
 key = 'key'
+columns = 'columns'
 print(f'{rid:<{5}} {key:<{5}} columns\n')
 
-for i in range(0,num ):
+for i in range(0, num):
   rec = table.get_latest_record(i)
   print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
 
-# print(table.disk)
-"""
+# Test Update
 print('\n')
-for i in range(0,num):
-  table.update_record(i, [None, i + 2, i + 3])
+for i in range(1,num+1):
+  query.update(i, None, i + 1000, i + 2000)
+  #table.update_record(i, [None, i + 2000, i + 3000])
 
-# print(table.disk)
+print(f'{rid:<{5}} {key:<{5}} columns & indirection\n')
 
-
-print(f'{rid:<{5}} {key:<{5}} columns\n')
-
-for i in range(0,num, 1000):
-#   pdb.set_trace()
+for i in range(0, num):
   rec = table.get_latest_record(i)
-  print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
-"""
+  print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns} {rec.indirection}')
 
-success1 = query.insert(11, 12, 13)
-# print(success1)
-#table.create_record([10001, 2002, 3003])
-rec = table.get_latest_record(10)
+print('\n')
+for i in range(1,num+1):
+  query.update(i, None, i + 1000, i + 2000)
+  #table.update_record(i, [None, i + 2000, i + 3000])
+
+print(f'{rid:<{5}} {key:<{5}} columns & indirection\n')
+
+for i in range(0, num):
+  rec = table.get_latest_record(i)
+  print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns} {rec.indirection}')
+
+# Insert Records Matching Values
+success1 = query.insert(21, 1010, 2010)
+rec = table.get_latest_record(30)
 print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
-
-#success2 = query.insert(10002, 2002, 4003)
-#table.create_record([10002, 2002, 4003])
-success2 = query.insert(12, 13, 14)
-rec = table.get_latest_record(11)
+success2 = query.insert(22, 18, 108)
+rec = table.get_latest_record(31)
 print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
 print(f"\n")
 
 # Test Locate\
 locate_column = 1
-locate_key = 10
+locate_key = 1010
 locate_records = query.index.indices[locate_column].get(locate_key)
 print("USING INDEX LOCATE:")
-print(f"RID's where column {locate_column} is {locate_key}: {locate_records}\n")
+print(f"BASE RID's where column {locate_column} is {locate_key}: {locate_records}\n")
 
 # Test Select
 select_records = query.select(locate_key, locate_column, [1, 1, 1])
 print("USING SELECT:")
-print(f"RID's where column {locate_column} is {locate_key}: {select_records}\n")
+print(f'{rid:<{5}} {key:<{5}} columns\n')
+for rec in select_records:
+  print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
+
+print(f"\n")
+
+# Test Select Version
+select_ver_records = query.select_version(locate_key, locate_column, [1, 1, 1], -3)
+print("USING SELECT VERSION:")
+print(f'{rid:<{5}} {key:<{5}} columns\n')
+for rec in select_ver_records:
+  print(f'{rec.rid:<{5}} {rec.key:<{5}} {rec.columns}')
